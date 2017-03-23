cd2713850d7ce64f148b524068cedce7f899ff5d
Calos Cen <carlosv1233@hotmail.com>
2017-03-23 16:58:06 -0600

    Obtener el volumen de una esfera por radio o por diametro

---
+++ 2-VolumenEsfera.hs
@@ -0,0 +1,7 @@
+--permite obtener el volumen de una esfera.
+--calcular el volumen por radio
+--"volumenRadio 5" (Es un ejemplo).
+volumenRadio r = (4/3)*pi*r^3
+--calcular el volumen por Diametro
+--"volumenRadio 10" (Es un ejemplo).
+volumenDiametro d=(4/3)*pi*(d/2)^3
