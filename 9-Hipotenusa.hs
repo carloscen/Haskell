d8cf525160362dbd3d94600287d8890b2595e314
Calos Cen <carlosv1233@hotmail.com>
2017-03-23 17:06:41 -0600

    Solicita dos argumentos de tipo Double para calcular la hipotenusa

---
+++ 9-Hipotenusa.hs
@@ -0,0 +1,7 @@
+--solicite dos argumentos de tipo Double para calcular la hipotenusa 
+--de un triángulo rectángulo y retorne un valor de tipo Double.
+--forma de llamar al metodo
+--"hipotenusa 2 4" (es un ejemplo).
+hipotenusa:: Double -> Double -> Double
+hipotenusa x y = sqrt(x^2 + y^2)
+
